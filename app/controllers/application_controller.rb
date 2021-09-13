f0b4a844bd107d9bd7ab3a80ea603b8eff7f332f
Samantha Laine <samanthalaine8@gmail.com>
2021-09-12 21:00:57 -0400

    completed assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,30 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
+  # get '/games' do
+  #   # get all the games from the database
+  #   games = Game.all
+  #   # return a JSON response with an array of all the game data
+  #   games.to_json
+  # end
 
-  get '/' do
-    { message: "Hello world" }.to_json
+  # get '/games' do
+  #   games = Game.all.order(:title)
+  #   games.to_json
+  # end
+
+  get '/games' do
+    games = Game.all.order(:title).limit(10)
+    games.to_json
   end
 
+  # use the :id syntax to create a dynamic route
+  get '/games/:id' do
+    # look up the game in the database using its ID
+    game = Game.find(params[:id])
+    # send a JSON-formatted response of the game data
+    game.to_json(include: { reviews: { include: :user } })
+  end
+
+
 end
+
